71d66abb8a8877cbd93381b0e39cc9ced38758b1
Legume Duprix <147527392+thelegumeduprix@users.noreply.github.com>
2023-11-18 14:20:22 +0100

    Fix formatting of file

--- src/computations.js
+++ src/computations.js
@@ -1,45 +1,45 @@
 function filterValidUserVotes(pollState) {
   return Object.entries(pollState.userVotes).reduce((result, [user, vote]) => {
-    if (vote !== "0") {
-      result[user] = vote;
+    if (vote !== '0') {
+      result[user] = vote
     }
-    return result;
-  }, {});
+    return result
+  }, {})
 }
 
 export function getWinningOptions(pollState) {
-  const voteCountsPerOption = getVoteCountsPerOption(pollState);
+  const voteCountsPerOption = getVoteCountsPerOption(pollState)
 
-  let winningOptions = [];
-  let winningVoteCount = 0;
+  let winningOptions = []
+  let winningVoteCount = 0
   Object.entries(voteCountsPerOption).forEach(([option, voteCount]) => {
     if (voteCount == winningVoteCount) {
-      winningOptions.push(option);
+      winningOptions.push(option)
     } else if (voteCount > winningVoteCount) {
-      winningVoteCount = voteCount;
-      winningOptions = [option];
+      winningVoteCount = voteCount
+      winningOptions = [option]
     }
-  });
+  })
 
-  return winningOptions;
+  return winningOptions
 }
 
 export function getVoteCountsPerOption(pollState) {
-  let voteCountsPerOption = {};
+  let voteCountsPerOption = {}
   Object.keys(pollState.options).forEach((option) => {
-    voteCountsPerOption[option] = 0;
-  });
+    voteCountsPerOption[option] = 0
+  })
 
-  const validUserVotes = filterValidUserVotes(pollState);
+  const validUserVotes = filterValidUserVotes(pollState)
 
   Object.values(validUserVotes).forEach((option) => {
-    voteCountsPerOption[option] += 1;
-  });
+    voteCountsPerOption[option] += 1
+  })
 
-  return voteCountsPerOption;
+  return voteCountsPerOption
 }
 
 export function getTotalVoteCount(pollState) {
-  const validUserVotes = filterValidUserVotes(pollState);
-  return Object.keys(validUserVotes).length;
+  const validUserVotes = filterValidUserVotes(pollState)
+  return Object.keys(validUserVotes).length
 }
