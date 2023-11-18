417651be6655cb24c215499829f6d42d2bb2fcfe
Legume Duprix <147527392+thelegumeduprix@users.noreply.github.com>
2023-11-18 14:09:18 +0100

    Add chat function for debugging

--- src/setup.js
+++ src/setup.js
@@ -22,6 +22,10 @@ export function setup() {
 
   if (DEBUG) {
     store.setDebugMode(true)
+
+    window.chat = (message, username = 'testuser', mod = true) => {
+      handleMessage({ mod, username }, message)
+    }
   }
 
   const client = tmi.Client({
