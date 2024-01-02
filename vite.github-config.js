7a11a2a080f5a603fb185f6cf5efd7af050b1550
Ruecklicht <149110238+ruecklicht@users.noreply.github.com>
2024-01-02 22:43:04 +0100

    Comment vite.github-config.js
    Add a little comment, so it is easier to see what this config is for.

--- vite.github-config.js
+++ vite.github-config.js
@@ -4,6 +4,8 @@ import defaultConfig from './vite.config.js';
 import { mergeConfig } from 'vite';
 
 // https://vitejs.dev/config/
+// This Vite configuration is used in GitHub Actions to create build artifacts (JS & CSS) 
+// with a fixed name for publishing though a CDN.
 export default mergeConfig(
   defaultConfig,
   {
